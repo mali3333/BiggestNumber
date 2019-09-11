5fb317a7368e3855c1cf2f9abb612cb5514b2cb1
Mir Ahmad Ali <mali3@ecc-jaguar.csuchico.edu>
2019-09-11 16:30:17 -0700

    The biggest number file will show the biggest number among the 2 entered

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -3,21 +3,33 @@
 
 int main()
 {
+        
   //declare variables here
+  int number1=0;
+  int number2=0;
+  int number3=0;
 
   std::cout<<"Please enter a whole number:\n";
   //get first input using std::cin>>
-
+  std::cin>>number1;
   std::cout<<"Please enter another whole number:\n";
   //get second input using std::cin>> again with a second variable
-
+  std::cin>>number2;
   //determine if first or second is bigger and store that in a third variable
+  if(number1>number2)
+  { 
+    number3=number1;
+  }
+  else
+  {
+    number3=number2;
+  }
 
-
-  std::cout<<"Of those two numbers, the biggest is: ";
+  std::cout<<"Of those two numbers, the biggest is:"<<number3;
   //print out the variable representing the biggest number
 
   std::cout<<std::endl<<"Thank you for playing.\n";
   
   return 0;
+
 }
